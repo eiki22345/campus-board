2e15e8ae2088b43e2ac95cfa5006736ac4848df9
eiki22345 <eiki22345@gmail.com>
2026-03-18 23:53:33 +0900

    fix:dopurifyのインポートをapp.jsから削除

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,9 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
-import DOMPurify from 'dompurify';
 
 window.Alpine = Alpine;
-window.DOMPurify = DOMPurify;
 
 Alpine.start();
 
-// 全てのいいねボタンを取得してイベントを設定
+
