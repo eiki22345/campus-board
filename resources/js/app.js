bba7137176b0bd06e88665501c71d9db8bb02652
eiki22345 <eiki22345@gmail.com>
2026-03-28 17:21:38 +0900

    refactor:Bootstrap・FontAwesomeをCDNからnpmローカル化 CDNへの外部依存を排除し、サプライチェーン攻撃のリスクを低減。
    - npm install bootstrap @fortawesome/fontawesome-free
    - resources/js/app.js に Bootstrap・FontAwesome の import を追加
    - app.blade.php から Bootstrap / FontAwesome Kit / Alpine.js の CDN タグを削除

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,9 @@
 import './bootstrap';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,9 +7,7 @@
      <meta name="csrf-token" content="{{ csrf_token() }}">
 
      <title>@yield('title')</title>
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-     <script src="https://kit.fontawesome.com/a251afe25c.js" crossorigin="anonymous"></script>
-     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.15.8/dist/cdn.min.js"></script>
+
      <link rel="manifest" href="{{ asset('manifest.json') }}">
      <link href="{{ asset('css/style.css')}}" rel="stylesheet">
      @vite(['resources/js/app.js'])
